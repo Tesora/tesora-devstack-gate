2741888fe19bdd3f2f6926e3c22fa873b10d4f2b
Brian Hunter <bhunter@tesora.com>
2015-02-19 22:51:44 -0500

    soft requirements mode
    I'm not sure this will work.
    
    Change-Id: Icea1de3e9c5bad980d54749c917eb557fa8c89f5

--- devstack-vm-gate-wrap.sh
+++ devstack-vm-gate-wrap.sh
@@ -334,7 +334,7 @@ export DEVSTACK_GATE_REQS_INTEGRATION=${DEVSTACK_GATE_REQS_INTEGRATION:-0}
 
 # Set to 1 if running the project is using libraries not specified
 # in global requirements
-export REQUIREMENTS_MODE=${REQUIREMENTS_MODE:-strict}
+export REQUIREMENTS_MODE=${REQUIREMENTS_MODE:-soft}
 
 # Set to 0 to disable clean logs enforcement (3rd party CI might want to do this
 # until they get their driver cleaned up)
